22b3f70754b045845cdb2b37c80e26e7393205ba
Guilherme Serpa Batista <serpa419@msn.com>
2019-11-21 11:58:23 -0300

    8 datatable works fine

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <?php
 include_once "header.php"
 ?>
-		<div class="main-panel">
-				<div class="content">
-					<div class="page-inner">
-						<div class="row">
-							<div class="col-md-12">
-								<div class="card">
-									<div class="card-header">
-										<!-- Start Content -->
-										<div class="card-title">TESTE</div>
-									</div>
-									<div class="card-action">
-									</div>
-									<!-- End Content -->
-								</div>
-							</div>
+<div class="main-panel">
+	<div class="content">
+		<div class="page-inner">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="card">
+						<div class="card-header">
+							<!-- Start Content -->
+							<div class="card-title">TESTE</div>
 						</div>
+						<div class="card-action">
+						</div>
+						<!-- End Content -->
 					</div>
 				</div>
-				
+			</div>
+		</div>
+	</div>
+
 <?php
 include_once "footer.php"
 ?>
