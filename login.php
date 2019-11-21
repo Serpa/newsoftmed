a21c0cf0d85d0d1e30626865b35c13920f226c41
la1608 <50717887+la1608@users.noreply.github.com>
2019-11-21 16:48:53 -0300

    11 logo site

--- login.php
+++ login.php
@@ -33,7 +33,7 @@
 <body class="login">
 	<div class="wrapper wrapper-login wrapper-login-full p-0">
 		<div class="login-aside w-50 d-flex flex-column align-items-center justify-content-center text-center bg-secondary-gradient">
-			<h1 class="title fw-bold text-white mb-3">SoftMED</h1>
+			<img src="img/logonome.png">
 			<p class="subtitle text-white op-7">SOFTMED-Sistema de gestão de Clínicas Médicas</p>
 		</div>
 		<div class="login-aside w-50 d-flex align-items-center justify-content-center bg-white">
