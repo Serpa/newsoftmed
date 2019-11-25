5e34fc5d1b058ccd029a5af03607ab50216c3a9d
la1608 <50717887+la1608@users.noreply.github.com>
2019-11-24 21:46:16 -0300

    19 commit - login

--- login.php
+++ login.php
@@ -34,11 +34,11 @@
 	<div class="wrapper wrapper-login wrapper-login-full p-0">
 		<div class="login-aside w-50 d-flex flex-column align-items-center justify-content-center text-center bg-secondary-gradient">
 			<img src="img/logonome.png">
-			<p class="subtitle text-white op-7">SOFTMED-Sistema de gestão de Clínicas Médicas</p>
+			<p class="subtitle text-white op-7">Sistema de gestão de Clínica Médica</p>
 		</div>
 		<div class="login-aside w-50 d-flex align-items-center justify-content-center bg-white">
 			<div class="container container-login container-transparent animated fadeIn">
-				<h3 class="text-center">Seja Bem-Vindo!</h3>
+				<h3 class="text-center">Seja bem-vindo!</h3>
 				<div class="login-form">
 					<div class="form-group">
 					<form action="dao/valida.php" method="POST">
